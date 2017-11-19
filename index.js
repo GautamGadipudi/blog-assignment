53adee5370ff6b30dd7ae81c4fa24b0141a90bf5
Gautam <gautam.96.gadipudi@gmail.com>
2017-11-19 11:36:32 +0530

    Updated Blog and BlogFeed API

--- index.js
+++ index.js
@@ -3,15 +3,16 @@ var http = require('http').Server(app);
 var bodyParser = require('body-parser');
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/mydb";
-const uuidv1 = require('uuid/v1');
-const queryString = require('query-string');
+var uuidv1 = require('uuid/v1');
+var URL = require('url');
 
 app.use(bodyParser.json());
 
 //User login/signup route
 app.post('/UserLogin', function(req, res){ //{username: "Gautam", password: "ggwp"}
 	var body = req.body;
-	MongoClient.connect(url, function(err, db) {
+	body.type = "user";
+ 	MongoClient.connect(url, function(err, db) {
 	  	if (err) throw err;
 	  	db.collection("users").find({username: body.username}).toArray(function(err, result){
 	  		
@@ -67,7 +68,7 @@ app.post('/NewBlog', function(req, res){	//{author: "Gautam", title: "blah", des
 			if(result.length){
 				db.collection('blogs').insertOne(body, function(err, result){
 					if (err) throw err;
-					console.log(body);
+					res.writeHead(200);
 					console.log(body.author + " blog posted.\n");
 					db.close();
 				})
@@ -82,25 +83,34 @@ app.post('/NewBlog', function(req, res){	//{author: "Gautam", title: "blah", des
 
 //GET all blogs
 app.get('/BlogFeed', function(req, res){
-	MongoClient.connect(url, function(err, db){
-		if (err) throw err;
-		db.collection('blogs').find({}).toArray(function(err, result){
+	
+		MongoClient.connect(url, function(err, db){
 			if (err) throw err;
-			res.body = result;
-			db.close();
+			db.collection('blogs').find({}).toArray(function(err, result){
+				if (err) throw err;
+				res.status(200).send(result);
+				
+				db.close();
+			})
 		})
-	})
+		
+	
+	
 })
 
 //GET details of single blog post (blog id from URL querystring)
-app.get('/Blog', function(req, res){	
-	var parsedQueryString = queryString.parse(location.search);
-	blog_id = parsedQueryString.blogid;
+app.get('/Blog', function(req, res){
+	var queryData = URL.parse(req.url, true).query;
 	MongoClient.connect(url, function(err, db){
 		if (err) throw err;
-		db.collection('blogs').find({_id: blog_id}).toArray(function(err, result){
+		db.collection('blogs').find({_id: queryData.blogid}).toArray(function(err, result){
 			if (err) throw err;
-			res.body = result;
+			if (result.length){
+				res.status(200).send(result);
+			}
+			else {
+				console.log("Invalid blog_id.\n");
+			}
 			db.close();
 		})
 	})
